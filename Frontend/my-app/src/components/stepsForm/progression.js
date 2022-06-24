68b9f40264d3f6b989e61f1efc2fcf28af83dfe1
Malo-LC <malolc2001@gmail.com>
2022-06-24 11:25:22 +0200

    center

--- Frontend/my-app/src/components/stepsForm/progression.js
+++ Frontend/my-app/src/components/stepsForm/progression.js
@@ -1,15 +1,18 @@
 import { React } from 'react';
 import LinearProgress from '@mui/material/LinearProgress';
+import Box from '@mui/material/Box';
 
 export function Progression({ step }) {
   return (
     <div className="Progression">
       <div className="Progress">
-        <LinearProgress
-          sx={{ width: 400 }}
-          variant="determinate"
-          value={((step * 1) / 3) * 100}
-        />
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <LinearProgress
+            sx={{ width: 400 }}
+            variant="determinate"
+            value={((step * 1) / 3) * 100}
+          />
+        </Box>
       </div>
     </div>
   );
